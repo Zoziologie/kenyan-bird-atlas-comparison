bf189fdb890b1314d15aa356fa386aaf093853ca
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-03-31 10:02:34 +0200

    Add babel and select2, and json loader for geojson

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,36 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
     },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
+    {
+      test: /(\.geojson$)/,
+      use: 'json-loader'
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
         }
-      ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, "src", "index.html")
-        })
-      ]
-  };
+      }
+    }
+  ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
