435ba5af63fe5a07d434d643af27fbba44f9fdde
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-03-31 07:32:03 +0200

    Add source files

--- src/index.js
+++ src/index.js
@@ -1,42 +1,94 @@
-import React, { useRef, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import mapboxgl from 'mapbox-gl';
+import syncMaps  from '@mapbox/mapbox-gl-sync-move';
+var $ = require( "jquery" );
+let colormap = require('colormap')
 
-mapboxgl.workerClass = MapboxWorker;
+
+import "./style.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+let dold
+let colormap_name = 'viridis'
+let style = 'mapbox://styles/mapbox/streets-v11'
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFmbnVzcyIsImEiOiIzMVE1dnc0In0.3FNMKIlQ_afYktqki-6m0g';
+var mapOLD = new mapboxgl.Map({
+    container: 'mapOLD',
+    style: style
+});
 
-const Map = () => {
-  const mapContainer = useRef();
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+var mapNEW = new mapboxgl.Map({
+    container: 'mapNEW',
+    style: style
+});
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
+syncMaps(mapOLD, mapNEW);
+mapOLD.fitBounds([[32.958984, -5.353521], [43.50585, 5.615985]])
 
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
+mapOLD.on('load', function () {
 
-    return () => map.remove();
-  }, []);
+    $.getJSON('assets/grid_w_sp.geojson',function(data){
+        dold = data
 
-  return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div className="map-container" ref={mapContainer} />
-    </div>
-  );
-};
+        mapOLD.addSource('grid_w_sp', {
+            type: 'geojson',
+            data: dold
+            });
+    
+        mapOLD.addLayer({
+            'id': 'grid_w_sp_l',
+            'type': 'fill',
+            'source': 'grid_w_sp',
+            'layout': {},
+            'paint': {
+                'fill-color': getFillColor(colormap_name),
+                'fill-opacity': 0.8
+            }
+        });
+    })
+});
 
-ReactDOM.render(<Map />, document.getElementById('app'));
+// Create a popup, but don't add it to the map yet.
+var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+     
+mapOLD.on('mousemove', 'grid_w_sp_l', function (e) {
+    mapOLD.getCanvas().style.cursor = 'pointer';
+    var prop = e.features[0].properties;
+    var description = '<b>grid:</b> ' + prop.SqN + prop.SqL+'<br>'+ '<b>Number of species:</b> ' + prop.sp_nb
+    popup.setLngLat(e.lngLat).setHTML(description).addTo(mapOLD);
+});
+     
+mapOLD.on('mouseleave', 'grid_w_sp_l', function () {
+    mapOLD.getCanvas().style.cursor = '';
+    popup.remove();
+});
+
+
+
+const getFillColor = (colormap_name) => {
+
+    let colors = colormap({
+        colormap: colormap_name,
+        //nshades: 10
+    })
+
+	var max = dold.features.reduce((acc,x) => Math.max(acc,x.properties.sp_nb),0);
+	var min = dold.features.reduce((acc,x) => Math.min(acc,x.properties.sp_nb),10000);
+
+	var c = ['interpolate',	['linear'],	['get', 'sp_nb']];
+
+	var step = (max - min) / (colors.length - 1);
+		for (var i = 0; i < colors.length; i++) {
+			c.push(min + (step * i))
+			c.push(colors[i])
+		}
+	return c
+}
+
+
+$.getJSON('assets/species.json',function(data){
+    spl = data
+})
