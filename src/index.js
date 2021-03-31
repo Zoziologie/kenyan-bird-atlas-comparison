a0fe06892e7bccb992b30394c11c8433e635df24
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-03-31 11:50:08 +0200

    Add spinner and link to other platform

--- src/index.js
+++ src/index.js
@@ -2,14 +2,37 @@ import mapboxgl from 'mapbox-gl';
 import syncMaps from '@mapbox/mapbox-gl-sync-move';
 require('select2')
 let $ = require("jquery");
+import {Spinner} from 'spin.js';
 let colormap = require('colormap')
 
 import "./style.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "select2/dist/css/select2.min.css";
+import "spin.js/spin.css";
 
 const oldAtlas = require('./grid_w_sp.geojson');
 const speciesList = require('./2019_checklist_of_the_birds_of_kenya_short.json');
+
+var opts = {
+    lines: 11, // The number of lines to draw
+    length: 78, // The length of each line
+    width: 20, // The line thickness
+    radius: 48, // The radius of the inner circle
+    scale: 1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    speed: 1.2, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-shrink', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000000', // CSS color or array of colors
+    fadeColor: '', // CSS color or array of colors
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    zIndex: 2000000000, // The z-index (defaults to 2e9)
+    className: 'spinner', // The CSS class to assign to the spinner
+    position: 'absolute', // Element positioning
+  };
 
 //oldAtlas.features.map()
 
@@ -53,6 +76,7 @@ mapOLD.on('load', function () {
 });
 
 mapNEW.on('load', function () {
+    new Spinner(opts).spin(document.getElementById('mapNEW'));
     $.getJSON('http://api.adu.org.za/sabap2/v2/coverage/project/kenya?period=&format=geoJSON', function(coverageKBM){
         mapNEW.addSource('grid_KBM', {
             type: 'geojson',
@@ -69,6 +93,8 @@ mapNEW.on('load', function () {
                 'fill-opacity': 0.8
             }
         });
+
+        $(".spinner").remove();
     })
 
 });
@@ -129,13 +155,6 @@ const getFillColor = (colormap_name,data,prop) => {
 
 
 $("#species").select2({
-    templateResult: function (species) {
-        //var baseUrl = "/user/pages/images/flags";
-        var $species = $(
-            '<span> ' + species.common_name + '</span>'
-        );
-        return $species;
-    },
     data: $.map(speciesList, function (obj) {
         obj.id = obj.sort
         obj.text = obj.common_name
@@ -145,6 +164,7 @@ $("#species").select2({
 
 $('#species').on('select2:select', function (e) {
     var spListData = e.params.data;
+    console.log(spListData)
     let oldAtlasSp = oldAtlas;
     oldAtlasSp.features.map( x=>{
         if (x.properties.sp_list.includes(spListData.ADU) ){
@@ -155,9 +175,20 @@ $('#species').on('select2:select', function (e) {
         return x
     })
     mapOLD.getSource('grid_w_sp').setData(oldAtlasSp);
-
+    new Spinner(opts).spin(document.getElementById('mapNEW'));
     $.getJSON('http://api.adu.org.za/sabap2/v2/summary/species/'+spListData.ADU+'/country/kenya?format=geoJSON',function (dataKBM){
         mapNEW.getSource('grid_KBM').setData(dataKBM);
+        $(".spinner").remove();
     })
-    
+
+    $('#ebird').attr("href","https://ebird.org/species/"+spListData['Clements--code']+"/KE")
+    $('#iucn').attr("href","https://apiv3.iucnredlist.org/api/v3/taxonredirect/"+spListData.IUCNtaxonID)
+    $('#kbm').attr("href","http://kenyabirdmap.adu.org.za/species_info.php?spp="+spListData.ADU)
+    $('#avibase').attr("href","https://avibase.bsc-eoc.org/species.jsp?avibaseid="+spListData.avibaseid)
+    $('#inaturalist').attr("href","https://www.inaturalist.org/observations?place_id=7042&taxon_id="+spListData.iNaturalisttaxonID)
+    $('#observado').attr("href","https://observation.org/species/"+spListData.ObservationorgID)
+    $('#gbif').attr("href","https://www.gbif.org/species/"+spListData.GBIFID)
 });
+
+
+  
