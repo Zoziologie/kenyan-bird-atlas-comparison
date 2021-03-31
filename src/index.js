4af994b8beeaf57f8a2e7950d9661155879907d3
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-03-31 10:02:54 +0200

    Add KBM and select species

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import mapboxgl from 'mapbox-gl';
-import syncMaps  from '@mapbox/mapbox-gl-sync-move';
-var $ = require( "jquery" );
+import syncMaps from '@mapbox/mapbox-gl-sync-move';
+require('select2')
+let $ = require("jquery");
 let colormap = require('colormap')
-
 
 import "./style.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import "select2/dist/css/select2.min.css";
 
-let dold
+const oldAtlas = require('./grid_w_sp.geojson');
+const speciesList = require('./2019_checklist_of_the_birds_of_kenya_short.json');
+
+//oldAtlas.features.map()
+
 let colormap_name = 'viridis'
 let style = 'mapbox://styles/mapbox/streets-v11'
 
@@ -23,72 +28,136 @@ var mapNEW = new mapboxgl.Map({
 });
 
 syncMaps(mapOLD, mapNEW);
-mapOLD.fitBounds([[32.958984, -5.353521], [43.50585, 5.615985]])
+mapOLD.fitBounds([
+    [32.958984, -5.353521],
+    [43.50585, 5.615985]
+])
 
 mapOLD.on('load', function () {
+    mapOLD.addSource('grid_w_sp', {
+        type: 'geojson',
+        data: oldAtlas
+    });
 
-    $.getJSON('assets/grid_w_sp.geojson',function(data){
-        dold = data
+    mapOLD.addLayer({
+        'id': 'grid_w_sp_l',
+        'type': 'fill',
+        'source': 'grid_w_sp',
+        'layout': {},
+        'paint': {
+            'fill-color': getFillColor(colormap_name,oldAtlas,'sp_nb'),
+            'fill-opacity': 0.8
+        },
+        "filter": ["!=", "sp_nb", 0]
+    });
+});
 
-        mapOLD.addSource('grid_w_sp', {
+mapNEW.on('load', function () {
+    $.getJSON('http://api.adu.org.za/sabap2/v2/coverage/project/kenya?period=&format=geoJSON', function(coverageKBM){
+        mapNEW.addSource('grid_KBM', {
             type: 'geojson',
-            data: dold
-            });
+            data: coverageKBM
+        });
     
-        mapOLD.addLayer({
-            'id': 'grid_w_sp_l',
+        mapNEW.addLayer({
+            'id': 'grid_KBM_l',
             'type': 'fill',
-            'source': 'grid_w_sp',
+            'source': 'grid_KBM',
             'layout': {},
             'paint': {
-                'fill-color': getFillColor(colormap_name),
+                'fill-color': getFillColor(colormap_name,coverageKBM,"full protocol"),
                 'fill-opacity': 0.8
             }
         });
     })
+
 });
 
+
+
 // Create a popup, but don't add it to the map yet.
-var popup = new mapboxgl.Popup({
+var popupOLD = new mapboxgl.Popup({
     closeButton: false,
     closeOnClick: false
 });
-     
 mapOLD.on('mousemove', 'grid_w_sp_l', function (e) {
     mapOLD.getCanvas().style.cursor = 'pointer';
     var prop = e.features[0].properties;
-    var description = '<b>grid:</b> ' + prop.SqN + prop.SqL+'<br>'+ '<b>Number of species:</b> ' + prop.sp_nb
-    popup.setLngLat(e.lngLat).setHTML(description).addTo(mapOLD);
-});
-     
-mapOLD.on('mouseleave', 'grid_w_sp_l', function () {
+    var description = '<b>grid:</b> ' + prop.SqN + prop.SqL + '<br>' + '<b>Number of species:</b> ' + prop.sp_nb
+    popupOLD.setLngLat(e.lngLat).setHTML(description).addTo(mapOLD);
+}).on('mouseleave', 'grid_w_sp_l', function () {
     mapOLD.getCanvas().style.cursor = '';
-    popup.remove();
+    popupOLD.remove();
+});
+
+var popupNEW = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+mapNEW.on('mousemove', 'grid_KBM_l', function (e) {
+    mapNEW.getCanvas().style.cursor = 'pointer';
+    var prop = e.features[0].properties;
+    var description = '<b>pentad:</b> ' + prop.pentad + '<br>' + '<b>Number of full protocol:</b> ' + prop['full protocol'] + '<br>' + '<b>Number of adhoc protocol:</b> ' + prop['adhoc protocol']
+    popupNEW.setLngLat(e.lngLat).setHTML(description).addTo(mapNEW);
+}).on('mouseleave', 'grid_KBM_l', function () {
+    mapNEW.getCanvas().style.cursor = '';
+    popupNEW.remove();
 });
 
 
-
-const getFillColor = (colormap_name) => {
+const getFillColor = (colormap_name,data,prop) => {
 
     let colors = colormap({
         colormap: colormap_name,
         //nshades: 10
     })
 
-	var max = dold.features.reduce((acc,x) => Math.max(acc,x.properties.sp_nb),0);
-	var min = dold.features.reduce((acc,x) => Math.min(acc,x.properties.sp_nb),10000);
+    var max = data.features.reduce((acc, x) => Math.max(acc, x.properties[prop]), 0);
+    var min = data.features.reduce((acc, x) => Math.min(acc, x.properties[prop]), 10000);
 
-	var c = ['interpolate',	['linear'],	['get', 'sp_nb']];
+    var c = ['interpolate', ['linear'],
+        ['get', prop]
+    ];
 
-	var step = (max - min) / (colors.length - 1);
-		for (var i = 0; i < colors.length; i++) {
-			c.push(min + (step * i))
-			c.push(colors[i])
-		}
-	return c
+    var step = (max - min) / (colors.length - 1);
+    for (var i = 0; i < colors.length; i++) {
+        c.push(min + (step * i))
+        c.push(colors[i])
+    }
+    return c
 }
 
 
-$.getJSON('assets/species.json',function(data){
-    spl = data
+$("#species").select2({
+    templateResult: function (species) {
+        //var baseUrl = "/user/pages/images/flags";
+        var $species = $(
+            '<span> ' + species.common_name + '</span>'
+        );
+        return $species;
+    },
+    data: $.map(speciesList, function (obj) {
+        obj.id = obj.sort
+        obj.text = obj.common_name
+        return obj
+    })
 })
+
+$('#species').on('select2:select', function (e) {
+    var spListData = e.params.data;
+    let oldAtlasSp = oldAtlas;
+    oldAtlasSp.features.map( x=>{
+        if (x.properties.sp_list.includes(spListData.ADU) ){
+            x.properties.sp_nb=1
+        } else{
+            x.properties.sp_nb=0
+        }
+        return x
+    })
+    mapOLD.getSource('grid_w_sp').setData(oldAtlasSp);
+
+    $.getJSON('http://api.adu.org.za/sabap2/v2/summary/species/'+spListData.ADU+'/country/kenya?format=geoJSON',function (dataKBM){
+        mapNEW.getSource('grid_KBM').setData(dataKBM);
+    })
+    
+});
